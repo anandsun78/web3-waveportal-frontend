6ae19573cf2bba92801cde708c422ac619b9c321
anandsun78 <45132372+anandsun78@users.noreply.github.com>
2022-09-09 12:27:44 -0600

    Updated UI

---
+++ src/common/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Navbar className="mb-4 border-bottom" bg="transparent" expand="lg">
+        <Navbar.Brand>Redflag Deals Tracker App</Navbar.Brand>
+        <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+        <Navbar.Collapse id="navbar-toggle">
+          <Nav className="ml-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </nav>
+  );
+};
+export default NavBar;
